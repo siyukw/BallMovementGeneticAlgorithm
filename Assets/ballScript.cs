ace23642af608e884aa86071c85c64b7909724fd
Siyu Kat Wang <siyukw@gmail.com>
2017-06-19 12:04:16 -0400

    added assets

---
+++ Assets/ballScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballScript : MonoBehaviour {
+	public float ballSpeed;
+	public Vector3 direction;
+
+	// Use this for initialization
+	// initializes speed and direction for each ball
+	void Start () {
+		ballSpeed = Random.Range (0.0f, 4.0f);
+		direction = Random.insideUnitCircle.normalized;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (direction * ballSpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/spawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour {
+	public GameObject[] balls;    
+
+	public void Start () {
+		// Start calling the Spawn function repeatedly after a delay .
+		// InvokeRepeating("Spawn", Time.deltaTime * 0.2);
+	}
+
+	void Spawn () {
+		// Instantiate a random enemy.
+		int ballIndex = Random.Range(0, balls.Length);
+		Instantiate(balls[ballIndex], transform.position, transform.rotation);
+	}
+}
